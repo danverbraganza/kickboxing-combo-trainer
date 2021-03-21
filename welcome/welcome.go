0c7803144c45ed2615d5c377ccad421e63dd7175
Danver Braganza <danverbraganza@gmail.com>
2021-03-21 00:21:49 -0700

    Contain and flex the options

--- welcome/welcome.go
+++ welcome/welcome.go
@@ -47,38 +47,40 @@ func (*WelcomePage) View(x moria.Controller) moria.View {
 		// Add more components here
 		m("div#disclaimer", nil, moria.S("Exercising is good for you! However, every individual is unique. By continuing to use this application, you recognize that you are taking full responsibility for the consequences. Make sure to check with your doctor before using this app if you need to. You agree that this is app is not responsible for any injuries.")),
 		m("div", nil,
-			m("select#select-duration", nil,
-				moria.F(func(children *[]moria.View) {
-					for _, duration := range []string{"1m", "2m", "3m"} {
-						*children = append(*children, m("option[value='"+duration+"']", nil, moria.S(duration)))
-					}
-				},
+			m("div#options", nil,
+				m("select#select-duration", nil,
+					moria.F(func(children *[]moria.View) {
+						for _, duration := range []string{"1m", "2m", "3m"} {
+							*children = append(*children, m("option[value='"+duration+"']", nil, moria.S(duration)))
+						}
+					},
+					),
 				),
-			),
-			m("select#select-speed", nil,
-				moria.F(func(children *[]moria.View) {
-					for _, speed := range []string{"slow", "medium", "fast"} {
-						*children = append(*children, m("option[value='"+speed+"']", nil, moria.S(speed)))
-					}
-				},
+				m("select#select-speed", nil,
+					moria.F(func(children *[]moria.View) {
+						for _, speed := range []string{"slow", "medium", "fast"} {
+							*children = append(*children, m("option[value='"+speed+"']", nil, moria.S(speed)))
+						}
+					},
+					),
 				),
-			),
-			m("button", js.M{
-				"config": mithril.RouteConfig,
-				"onclick": func() {
-					mithril.RouteRedirect(
-						strings.Join([]string{
-							"",
-							"round",
-							w.Duration.String(),
-						},
-							"/")+"/selectedCombos="+w.SelectedCombosAsString(),
+				m("button", js.M{
+					"config": mithril.RouteConfig,
+					"onclick": func() {
+						mithril.RouteRedirect(
+							strings.Join([]string{
+								"",
+								"round",
+								w.Duration.String(),
+							},
+								"/")+"/selectedCombos="+w.SelectedCombosAsString(),
 
-						js.M{},
-						false,
-					)
-				}},
-				moria.S("Go!"),
+							js.M{},
+							false,
+						)
+					}},
+					moria.S("Go!"),
+				),
 			),
 		),
 
